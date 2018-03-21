182cbcc3a974499823e1ca27183f544b44e358fb
王者伟业 <wang1902568721@163.com>
2018-03-21 14:32:21 +0800

    增加对话框测试应用程序

---
+++ AboutDlg/AboutDlgDlg.h
@@ -0,0 +1,33 @@
+
+// AboutDlgDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CAboutDlgDlg 对话框
+class CAboutDlgDlg : public CDialogEx
+{
+// 构造
+public:
+	CAboutDlgDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTDLG_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
