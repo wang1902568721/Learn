23d1b52ef86ce503d54561d8b4097a92aa7cbed6
王者伟业 <wang1902568721@163.com>
2018-04-19 17:54:29 +0800

    增加变量属性测试项目

---
+++ AboutVar/src.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include "head.h"
+
+extern int iNum;
+extern  int iNum2;
+
+int main()
+{
+	cout << iNum << endl;
+	cout << iNum2 << endl;
+	system("pause");
+	return 0;
+}
---
+++ AboutVar/subSrc.cpp
@@ -0,0 +1,1 @@
+extern int iNum2 = 20;
